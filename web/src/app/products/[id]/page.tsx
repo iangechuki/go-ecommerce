79fb8638789945dfac0438e22209406ce1d06737
Ian Gechuki <iangechuki@gmail.com>
2025-03-02 00:37:39 +0300

    Remove web folder from main repo to prepare submodule

--- web/src/app/products/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductPage from "./productPage";
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
-export default async function Page({ params }: ProductPageProps){
-    return <ProductPage id={params.id}/>
-}
--- web/src/constants/categories.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "@/types";
-
-export const categories:Category[] = [
-    { name: "Electronics", slug: "electronics" },
-    { name: "Fashion", slug: "fashion" },
-    { name: "Home & Kitchen", slug: "home" },
-  ];
